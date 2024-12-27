6d2d90785a0f0d70f2b45f556cc1d1fbca247a91
Henri Lehtonen <henri.lehtonen.87@gmail.com>
2024-12-27 12:44:29 +0100

    Modal confirmation added for deleting pets and quotes.

--- AppGoodFriendsRazor/Pages/FriendDetails.cshtml.cs
+++ AppGoodFriendsRazor/Pages/FriendDetails.cshtml.cs
@@ -9,7 +9,12 @@ namespace MyApp.Namespace
     {
         private readonly IFriendsService _service;
         public string? ErrorMessage { get; set; } = null;
+
+        //Skapa input model?
         public IFriend Friend { get; set; }
+
+        [BindProperty]
+        public Guid FriendId { get; set; }
         public async Task<IActionResult> OnGet()
         {
             try
@@ -19,6 +24,7 @@ namespace MyApp.Namespace
 
                 //Use the Service
                 Friend = await _service.ReadFriendAsync(_id, false);
+                FriendId = Friend.FriendId;
             }
             catch (Exception e)
             {
@@ -26,10 +32,47 @@ namespace MyApp.Namespace
             }
             return Page();
         }
-    
-    public FriendDetailsModel(IFriendsService service)
+
+        public async Task<IActionResult> OnPostDeletePet(Guid id)
+        {
+
+            //Set the Quote as deleted, it will not be rendered
+
+            try
+            {
+                await _service.DeletePetAsync(id);
+                Friend = await _service.ReadFriendAsync(FriendId, false);
+            }
+            catch (Exception e)
+            {
+                ErrorMessage = e.Message;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostDeleteQuote(Guid id)
+        {
+
+            //Set the Quote as deleted, it will not be rendered
+
+            try
+            {
+                await _service.DeleteQuoteAsync(id);
+                Friend = await _service.ReadFriendAsync(FriendId, false);
+            }
+            catch (Exception e)
+            {
+                ErrorMessage = e.Message;
+            }
+            return Page();
+        }
+
+
+        public FriendDetailsModel(IFriendsService service)
         {
             _service = service;
         }
+
     }
+
 }
